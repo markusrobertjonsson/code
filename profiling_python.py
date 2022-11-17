5b11cf905da4d6e4a498388a31b870f74a46fd64
Markus Jonsson <30292486+markusrobertjonsson@users.noreply.github.com>
2022-11-17 22:26:47 +0100

    Update profiling_python.py

--- profiling_python.py
+++ profiling_python.py
@@ -12,3 +12,9 @@ s = io.StringIO()
 ps = pstats.Stats(pr, stream=s).sort_stats(SortKey.CUMULATIVE)
 ps.print_stats(10)
 print(s.getvalue())
+
+
+# ALT
+# with cProfile.Profile() as pr:
+#     DO STUFF
+#     pr.sort_stats(SortKey.CUMULATIVE).print_stats(20)
